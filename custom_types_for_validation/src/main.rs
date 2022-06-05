ee80f9fe82b17f04cfd02628ff94fc5f7e7bdddf
Henry Wang <henrywang97@gmail.com>
2022-06-05 02:00:21 +0100

    yayaya

--- custom_types_for_validation/src/main.rs
+++ custom_types_for_validation/src/main.rs
@@ -1,17 +1,25 @@
-struct Guess {
-    value: i32
-}
+mod guess {
+    pub struct Guess {
+        value: i32,
+    }
 
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}", value);
+    impl Guess {
+        pub fn new(value: i32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {}", value);
+            }
+            Guess { value }
         }
-        Guess { value }
+
+        pub fn value(&self) -> i32 {
+            // getter
+            self.value
+        }
     }
 }
 
 fn main() {
-    let g1 = Guess::new(50);
-    let g2 = Guess::new(101);
+    let g1 = guess::Guess::new(50);
+    println!("{}", g1.value());
+    let g2 = guess::Guess::new(101);
 }
